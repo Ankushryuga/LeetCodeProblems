abc5d49f5eef33094ec625f092b847392915e314
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-09-14 08:54:11 +0530

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,1 +1,17 @@
 
+// Objective: given an array and an integer, return array indices whose sum is equal to target.
+// example: i/p: [1,2,3,4,5].  o/p: [2,4]
+
+//Java code:
+
+class twoSum{
+  public int[] sum(int arr[], int target){
+    for(int i=0;i<arr.length;i++){
+      for(int j=i+1;j<arr.length;j++){
+          if(arr[i]+arr[j]==target){
+          return new int[]{i,j};
+          }
+      }
+    }
+  }
+}
