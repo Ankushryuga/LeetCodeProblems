bbcfb18389b78cd97f03682d0885b2e19cc77b24
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-12-05 18:18:45 +0530

    Create LeetCodeProblem876.cpp

---
+++ LeetCodeProblem876.cpp
@@ -0,0 +1,5 @@
+// Middle of linked list.
+      
+// Input: head = [1,2,3,4,5] Output: [3,4,5] Explanation: The middle node of the list is node 3. Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
