c601c14103ba49b2932ad17fc3c41145482ba9db
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-12-06 14:30:22 +0530

    Update LeetCodeProblem876.cpp

--- LeetCodeProblem876.cpp
+++ LeetCodeProblem876.cpp
@@ -3,7 +3,7 @@
 // Input: head = [1,2,3,4,5] Output: [3,4,5] Explanation: The middle node of the list is node 3. Input: head = [1,2,3,4,5,6]
 // Output: [4,5,6]
 // Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
-
+// test
 
 ListNode* middleNode(ListNode* head){
             // head is given inside method.
