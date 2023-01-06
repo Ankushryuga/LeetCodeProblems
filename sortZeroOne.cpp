9c784edec9639e59a53560a46c70625079e893e9
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-01-06 10:10:46 +0530

    Update sortZeroOne.cpp

--- sortZeroOne.cpp
+++ sortZeroOne.cpp
@@ -38,3 +38,61 @@ sortZeroOne(arr);
 
 return 0;
 }
+
+// sorting zeros, ones and two in c++ programming.
+
+##################      sort 0,1,& 2 in c++ programming.        ###########################
+
+#include<bits/stdc++.h>
+using namespace std;
+/*
+void sortZeroOne(vector<int>&array){
+    int countZero=0;
+    for(int i=0;i<array.size();i++){
+        if(array[i]==0){
+            countZero++;
+        }
+    }
+    for(int i=0;i<countZero;i++){
+        array[i]=0;
+    }
+    for(int i=countZero;i<array.size();i++){
+        array[i]=1;
+    }
+}
+*/
+void sortZeroOneTwo(vector<int>&array){
+    int countZero=0;
+    int countOne=0;
+    for(int i=0;i<array.size();i++){
+        if(array[i]==0){
+            countZero++;
+        }
+        else if(array[i]==1){
+            countOne++;
+        }
+    }
+    for(int i=0;i<countZero;i++){
+        array[i]=0;
+    }
+    for(int i=countZero;i<countZero+countOne;i++){
+        array[i]=1;
+    }
+    for(int i=countZero+countOne;i<array.size();i++){
+        array[i]=2;
+    }
+}
+int main(){
+    vector<int>array{1,1,1,1,0,0,0,1,0,1,0,2,2,2,2,2,2,0,0,0,1,1,1,0,0};   
+    /*
+    0's -10;
+    1's -9;
+    2's -6;
+    */
+    
+    sortZeroOneTwo(array);
+    for(int i=0;i<array.size();i++){
+        cout<<array[i]<<" ";
+    }
+    return 0;
+}
