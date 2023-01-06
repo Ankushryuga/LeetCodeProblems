d778b34426ba6b7d43a3233d1c8f5e24ce5c8754
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-01-06 17:26:53 +0530

    Update LeetCodeProblem1833.cpp

--- LeetCodeProblem1833.cpp
+++ LeetCodeProblem1833.cpp
@@ -21,9 +21,10 @@ int maxIceCreams(vector<int>&costs, int coins){
 int maxIceCreams(vector<int>&costs, int coins){
     sort(costs.begin(), costs.end());
     for(int i=0;i<costs.size();i++){
-      if(coins>costs[i]){
+      if(coins>=costs[i]){
         coins=coins-costs[i];
       }
+      else
       return i;
     }
   return costs.size();
