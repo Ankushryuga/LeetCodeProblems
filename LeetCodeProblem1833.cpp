2c329d0c780490f803203a31d89eb9f4ec3c33db
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-01-06 17:26:19 +0530

    Update LeetCodeProblem1833.cpp

--- LeetCodeProblem1833.cpp
+++ LeetCodeProblem1833.cpp
@@ -14,8 +14,6 @@ int maxIceCreams(vector<int>&costs, int coins){
   }
   return costs.size();
 }
-
-
 // RECALLING maximum number of icecream we can get with certain amount of coins.
 // example:  A=[1,3,2,4,1], coins=7.
 // return maximum number of icecream we can get.
@@ -23,15 +21,10 @@ int maxIceCreams(vector<int>&costs, int coins){
 int maxIceCreams(vector<int>&costs, int coins){
     sort(costs.begin(), costs.end());
     for(int i=0;i<costs.size();i++){
-      if(costs[i]<coins){
-        coins-=cost[i];
+      if(coins>costs[i]){
+        coins=coins-costs[i];
       }
-      else
       return i;
     }
-
   return costs.size();
-
-
-
 }
